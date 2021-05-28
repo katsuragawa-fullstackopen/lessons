ae298638bbdc2a7366df1ca84f887b6208d867c5
André <andre.katsuragawa@gmail.com>
2021-05-28 17:28:29 -0300

    Update notes to part 2.b

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  /* const hook = () => {
+    console.log("effect");
+    const eventHandler = (response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    };
+    const promise = axios.get("http://localhost:3001/notes");
+    promise.then(eventHandler);
+  }; 
+  useEffect(hook, []); */
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+
+  console.log("render", notes.length, "notes");
 
   const addNote = (e) => {
     e.preventDefault();
@@ -16,17 +38,14 @@ const App = (props) => {
     };
     console.log(noteObj);
     setNotes(notes.concat(noteObj));
-    console.log(notes.concat(noteObj));
     setNewNote("");
   };
 
   const handleNoteChange = (e) => {
     setNewNote(e.target.value);
-    console.log(e.target.value);
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
-  console.log(notesToShow);
 
   return (
     <div className="container">
