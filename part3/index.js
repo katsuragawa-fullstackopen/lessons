8f51be1c5894944ac0e6bafae9c90f8b06e53280
André <andre.katsuragawa@gmail.com>
2021-06-02 17:11:59 -0300

    Add index.js

--- part3/index.js
+++ part3/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const { createServer } = require("http"); // import http - CommonJS format
+
+/* create new web server, register an event handler that's called every time
+an HTTP request it's made to the server */
+const app = createServer((reQuest, response) => {
+  // respond with the status code 200, content-type header is set to text/plain
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  // return hello world
+  response.end("Hello World");
+});
+
+// bind the http server to app variable
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
